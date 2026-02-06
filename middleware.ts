9a9409432043e51dbe86b9bcc74041af45f0dc84
Abhisha Kumar <100290304+immiGrow@users.noreply.github.com>
2026-02-06 23:42:10 +0530

    final fixes for site

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 // middleware.ts
-export const runtime = 'edge'; 
+
+// CHANGE THIS LINE from 'edge' to 'experimental-edge'
+export const runtime = 'experimental-edge'; 
 
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
