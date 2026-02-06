6dd0cd1906c8af20f27874c78fdd5901b4416220
Abhisha Kumar <100290304+immiGrow@users.noreply.github.com>
2026-02-06 12:32:55 +0530

    final fixes

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,11 @@
-const config = {
-  default: {
-    deployment: {
-      provider: "cloudflare-workers",
-    },
-  },
-};
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
+import d1TagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
 
-export default config;
+export default defineCloudflareConfig({
+  // Provide valid override implementations here:
+  incrementalCache: r2IncrementalCache,
+  queue: doQueue,
+  tagCache: d1TagCache,
+});
